567ca1dfdef17aee272a28658ec44b06b982503e
ehime <dodomeki@gmail.com>
2014-03-27 10:03:13 -0700

    Changing namespace to be abstract, Adding Abstract Model and Modeling stub

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,7 +3,7 @@
 var app = angular.module('modelshareApp');
 
 // http://www.mocky.io/v2/5334508f6c7507900e8e275c
-app.factory('DefaultResource', function($resource) {
+app.factory('AbstractResource', function($resource) {
 
     var baseURL = 'http://www.mocky.io',
         baseParams =  {
@@ -20,5 +20,11 @@ app.factory('DefaultResource', function($resource) {
             p = _.extend(params,  baseParams),
             o = _.extend(options, baseOptions);
         return $resource(url, p, o);
+    };
+});
+
+app.factory('AbstractModel', function() {
+    var Model = function() {
+        
     };
 });
