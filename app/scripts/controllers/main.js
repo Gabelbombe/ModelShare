83da2b47454771003e8d3a6a279bf2a7a97cefe4
ehime <dodomeki@gmail.com>
2014-03-27 09:24:29 -0700

    Adding Mocky response uri

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,7 @@
 
 var app = angular.module('modelshareApp');
 
+// http://www.mocky.io/v2/5334508f6c7507900e8e275c
 app.factory('DefaultResource', function($resource) {
 
     var baseURL = 'http://www.mocky.io',
